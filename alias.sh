fdb3bae182bc6b847d8d7d3bf94b830780af5117
Nillows <133193557+Nillows@users.noreply.github.com>
2024-08-23 15:19:41 -0400

    Create alias.sh

---
+++ alias.sh
@@ -0,0 +1,31 @@
+# HOW TO USE
+# PLACE THIS CODE BLOCK SOMEWHERE IN YOUR '.bashrc' FILE AND THEN EXECUTE 'source ~/.bashrc' OR OPEN A FRESH TERMINAL
+
+# Creates a history array containing directories I have visited
+# edits the 'cd' command to also perform a 'PWD' call and append to history array
+# establishes the 'back' command to perform the proper 'cd' command to go reverse your traversal
+# deletes the last entry in the history array to ensure the history array remains accurate 
+
+# Initialize the pathHistory array and arrayPosition
+pathHistory=("$HOME")
+arrayPosition=0
+
+# Function to override the 'cd' command
+cd() {
+    builtin cd "$@" && {  # Perform the actual cd operation
+        PWD=$(pwd)       # Get the new directory path
+        pathHistory+=("$PWD") # Add the new path to pathHistory
+        ((arrayPosition++)) # Increment the position
+    }
+}
+
+# Function to go back to the previous directory and truncate history
+back() {
+    if [[ $arrayPosition -gt 0 ]]; then
+        ((arrayPosition--)) # Decrement the position
+        builtin cd "${pathHistory[$arrayPosition]}" || ((arrayPosition++)) # Go to the previous directory OR correct position on failure
+        unset pathHistory[arrayPosition+1] # Remove the last entry in pathHistory
+    else
+        echo "No previous directory in history."
+    fi
+}
