b0db340a127423b0e532cbd7cf67e45412d9af29
Nillows <133193557+Nillows@users.noreply.github.com>
2024-08-23 16:35:58 -0400

    Update alias.sh
    updated comments

--- alias.sh
+++ alias.sh
@@ -1,10 +1,15 @@
 # HOW TO USE
-# PLACE THIS CODE BLOCK SOMEWHERE IN YOUR '.bashrc' FILE AND THEN EXECUTE 'source ~/.bashrc' OR OPEN A FRESH TERMINAL
+# PLACE THIS CODE BLOCK SOMEWHERE IN YOUR '.bashrc' FILE AND THEN EXECUTE 'source ~/.bashrc' OR OPEN A NEW TERMINAL
+# execute alias 'back' to go back to previous directory. This alias is designed to work with and compliment the standard command 'cd ..' 
+
+
 
 # Creates a history array containing directories I have visited
 # edits the 'cd' command to also perform a 'PWD' call and append to pathHistory array
-# establishes the 'back' command to perform the proper 'cd' command to reverse your traversal path so far
+# establishes the 'back' command to perform the proper 'cd' command to reverse your working traversal path so far
 # deletes the last entry in the history array to ensure the history array remains accurate from $HOME to $PWD
+# automatically increments and decrements position in pathHistory array.
+# execute alias 'back' to go back to previous directory. This command is meant to work with and compliment the standard command 'cd ..' 
 
 # Initialize the pathHistory array and arrayPosition
 pathHistory=("$HOME")
