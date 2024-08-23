ada41d2fa82d081d4c8310cdfc7a3c997e127d29
Nillows <133193557+Nillows@users.noreply.github.com>
2024-08-23 16:38:30 -0400

    Update alias.sh

--- alias.sh
+++ alias.sh
@@ -3,13 +3,12 @@
 # execute alias 'back' to go back to previous directory. This alias is designed to work with and compliment the standard command 'cd ..' 
 
 
-
 # Creates a history array containing directories I have visited
 # edits the 'cd' command to also perform a 'PWD' call and append to pathHistory array
 # establishes the 'back' command to perform the proper 'cd' command to reverse your working traversal path so far
 # deletes the last entry in the history array to ensure the history array remains accurate from $HOME to $PWD
 # automatically increments and decrements position in pathHistory array.
-# execute alias 'back' to go back to previous directory. This command is meant to work with and compliment the standard command 'cd ..' 
+
 
 # Initialize the pathHistory array and arrayPosition
 pathHistory=("$HOME")
